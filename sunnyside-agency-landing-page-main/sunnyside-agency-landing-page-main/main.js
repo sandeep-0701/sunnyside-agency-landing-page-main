d5b65b927177a324fb54e1b12a4cce180b4fa341
Sandeep Guttula <81863963+sandeep-0701@users.noreply.github.com>
2022-03-06 17:13:12 +0530

    Delete sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/

--- sunnyside-agency-landing-page-main/sunnyside-agency-landing-page-main/main.js
+++
@@ -1,7 +0,0 @@
-const navToggle  = document.querySelector('#navToggle')
-const nav  = document.querySelector('.menu');
-
-
-navToggle.addEventListener('click',() => {
-    nav.classList.toggle('open');
-})
